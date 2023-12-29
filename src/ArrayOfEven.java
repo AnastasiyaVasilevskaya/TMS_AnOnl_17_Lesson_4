8377ed1a86bbc3d9bf7ede016939379125c78645
Anastasiya Vasilevskaya <nastena2004vasil@gmail.com>
2023-12-30 01:42:36 +0300

    task4. adding even elements to a new array

---
+++ src/ArrayOfEven.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayOfEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите размер массива (от 6 до 10 включительно): ");
+        int size;
+        do {
+            while (!scanner.hasNextInt()) {
+                System.out.println("ОТ 6 ДО 10!! 6 7 8 9 10. Повторите ввод!");
+                scanner.next();
+            }
+            size = scanner.nextInt();
+            if (size < 6 || size > 10) {
+                System.out.println("ОТ 6 ДО 10!! 6 7 8 9 10. Повторите ввод!");
+            }
+        } while (size < 6 || size > 10);
+
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 50);
+        }
+        System.out.println(Arrays.toString(array));
+
+        int count = 0;
+        for (int number : array) {
+            if (number % 2 == 0) {
+                count++;                    //считаю четные элементы в массиве1
+            }
+        }
+
+        int[] arrayOfEven = new int[count];
+        for (int j = 0, readNumber = 0; readNumber < array.length; readNumber++) {
+            if (array[readNumber] % 2 == 0) {
+                arrayOfEven[j++] = array[readNumber];
+            }
+        }
+        System.out.println(Arrays.toString(arrayOfEven));
+    }
+}
